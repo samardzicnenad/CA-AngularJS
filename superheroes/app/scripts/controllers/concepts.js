d13ec447b2e60fcfe75d50f7993f1729ffd026ec
Nenad Samardzic <samardzicnenad@gmail.com>
2015-12-04 17:12:38 -0500

    superheroes: removed logging to the console

--- superheroes/app/scripts/controllers/concepts.js
+++ superheroes/app/scripts/controllers/concepts.js
@@ -2,7 +2,6 @@
 
 angular.module('Superheroes').controller('ConceptsController', ['$scope', 'comicVine', function($scope, comicVine ) {
     comicVine.getData("concepts", "filter=name:life&sort=name:asc").then(function(data) {
-        console.log(data.results[0]);
         $scope.concepts = data.results;
     });
 }]);
