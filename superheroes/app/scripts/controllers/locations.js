7b824006c5e0fabaccdbfe5428efa477b7bb103a
Nenad Samardzic <samardzicnenad@gmail.com>
2015-12-05 01:21:15 -0500

    superheroes: Added 'Origins' page

--- superheroes/app/scripts/controllers/locations.js
+++ superheroes/app/scripts/controllers/locations.js
@@ -2,7 +2,6 @@
 
 angular.module('Superheroes').controller('LocationsController', ['$scope', 'comicVine', function($scope, comicVine ) {
     comicVine.getData("locations", "filter=name:island&sort=name:asc").then(function(data) {
-        console.log(data.results[0]);
         $scope.locations = data.results;
     });
 }]);
--- superheroes/app/scripts/controllers/objects.js
+++ superheroes/app/scripts/controllers/objects.js
@@ -2,7 +2,6 @@
 
 angular.module('Superheroes').controller('ObjectsController', ['$scope', 'comicVine', function($scope, comicVine ) {
     comicVine.getData("objects", "filter=name:sword&sort=name:asc").then(function(data) {
-        console.log(data.results[0]);
         $scope.objects = data.results;
     });
 }]);
